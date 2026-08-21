d70d47093308aeb864e9d6d2d10dfe97f7b1645f
Mbanjafar <m.banjafar@gmail.com>
2026-08-21 16:13:50 +0200

    Fix Yeni İstanbul A2 hero image for GitHub Pages

--- yeni-istanbul-a2/sw.js
+++ yeni-istanbul-a2/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "yeni-istanbul-a2-unit-1-v1";
-const ASSETS = ["./", "./index.html", "./styles.css", "./data.js", "./app.js", "./icon.svg", "./manifest.webmanifest", "./assets/unit-1-gezelim-gorelim.png"];
+const CACHE = "yeni-istanbul-a2-unit-1-v2";
+const ASSETS = ["./", "./index.html", "./styles.css", "./data.js", "./app.js", "./icon.svg", "./manifest.webmanifest", "./assets/unit-1-gezelim-gorelim.jpg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
